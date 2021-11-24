ada15400b2ad4525aff2ee0d5700fda1bd41dc73
pleed0215 <pleed0215@gmail.com>
2021-11-24 17:44:33 +0900

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "udemy-gatsby",
-  },
-  plugins: [],
+    siteMetadata: {
+        siteUrl: 'https://www.yourdomain.tld',
+        title: 'udemy-gatsby',
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-typescript`,
+            options: {
+                isTSX: true, // defaults to false
+                jsxPragma: `jsx`, // defaults to "React"
+                allExtensions: true, // defaults to false
+            },
+        },
+        'gatsby-plugin-sass',
+    ],
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+// markup
+const IndexPage = () => {
+    return (
+        <main>
+            <title>Hello, gatsby</title>
+            <h1>Hello</h1>
+        </main>
+    );
+};
+
+export default IndexPage;
